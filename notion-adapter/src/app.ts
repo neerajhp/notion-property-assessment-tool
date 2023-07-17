c4d7d56a7ca92953949b7714797ef20f8cb28856
Neeraj Patel <neeraj.patel@rea-group.com>
2023-07-17 21:30:53 +1000

    Update SAM template to  deploy Typescript project

---
+++ notion-adapter/src/app.ts
@@ -0,0 +1,6 @@
+/**
+ * A Lambda function that logs the payload received from a CloudWatch scheduled event.
+ */
+export const handler = async (event, context) => {
+  console.info("It works");
+};
--- src/handlers/scheduled-event-logger.mjs
+++
@@ -1,6 +0,0 @@
-/**
- * A Lambda function that logs the payload received from a CloudWatch scheduled event.
- */
-export const scheduledEventLoggerHandler = async (event, context) => {
-    console.info(JSON.stringify(event));
-}
